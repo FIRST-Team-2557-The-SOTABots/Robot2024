bfc6fad0105911867af8eb3e0d29beb5d1e19cbd
HoJo42 <howardwalz@gmail.com>
2024-03-07 18:34:28 -0800

    Rotates to the proper angle

--- src/main/java/frc/robot/commands/Trap.java
+++ src/main/java/frc/robot/commands/Trap.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer.LimeLightPipelines;
 import frc.robot.commands.swerve.RotateToAngle;
@@ -9,20 +11,43 @@ import frc.robot.subsystems.SOTA_SwerveDrive;
 public class Trap extends SequentialCommandGroup {
 
     public enum TrapGyroAngles {
-        STAGE_LEFT(0),
-        STAGE_RIGHT(0),
-        BACK_STAGE(180);
+        STAGE_LEFT(120),
+        STAGE_RIGHT(240),
+        CENTER_STAGE(0),
+        ERROR(0);
 
         public double angle;
 
-        TrapGyroAngles(double angle){
+        TrapGyroAngles(double angle) {
             this.angle = angle;
         }
-    } 
+    }
 
     public Trap(SOTA_SwerveDrive drive) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(LimeLightPipelines.STAGE.id);
-        addCommands(new RotateToAngle(drive, TrapGyroAngles.BACK_STAGE.angle));
+
+        addCommands(new RotateToAngle(drive, this::calcTargetAngle));
+
         addRequirements(drive);
     }
+
+    public double calcTargetAngle() {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline")
+                .setNumber(LimeLightPipelines.STAGE.id);
+        long tagID = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid")
+                .getInteger(-1);
+        TrapGyroAngles targetAngle;
+        if (tagID == 12 || tagID == 16) {
+            targetAngle = TrapGyroAngles.STAGE_RIGHT;
+            SmartDashboard.putBoolean("Worked", true);
+        } else if (tagID == 11 || tagID == 15) {
+            targetAngle = TrapGyroAngles.STAGE_LEFT;
+        } else if (tagID == 13 || tagID == 14) {
+            targetAngle = TrapGyroAngles.CENTER_STAGE;
+        } else {
+            SmartDashboard.putBoolean("Worked", false);
+            targetAngle = TrapGyroAngles.ERROR;
+        }
+        return targetAngle.angle;
+    }
+
 }
--- src/main/java/frc/robot/commands/swerve/RotateToAngle.java
+++ src/main/java/frc/robot/commands/swerve/RotateToAngle.java
@@ -1,24 +1,39 @@
 package frc.robot.commands.swerve;
 
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SOTA_SwerveDrive;
 
 public class RotateToAngle extends Command {
     private SOTA_SwerveDrive mDrive;
     private PIDController mPidController;
+    private DoubleSupplier targetAngle;
 
-    public RotateToAngle(SOTA_SwerveDrive drive, double targetAngle) {
+    public RotateToAngle(SOTA_SwerveDrive drive, DoubleSupplier targetAngle) {
         this.mDrive = drive;
+        this.targetAngle = targetAngle;
         this.mPidController = new PIDController(0.12, 0.0, 0.01);
         mPidController.setTolerance(0.5);
-        mPidController.setSetpoint(targetAngle);
+    }
+
+    @Override
+    public void initialize() {
+        SmartDashboard.putNumber("RotateToAngle", targetAngle.getAsDouble());
+        mPidController.setSetpoint(targetAngle.getAsDouble());
     }
 
     @Override
     public void execute() {
-        mDrive.drive(new ChassisSpeeds(0, 0, mPidController.calculate(mDrive.getHeading().getDegrees())));
+        mDrive.drive(new ChassisSpeeds(0, 0, mPidController.calculate(wrapHeading(mDrive.getHeadingDegrees()))));
+    }
+
+    public double wrapHeading(double angle) {
+        return MathUtil.inputModulus(angle, 0, 360);
     }
 
     @Override
