5e8390c5632e6f3d7c4ab3b35fb7339bc71aded8
HoJo42 <howardwalz@gmail.com>
2024-03-28 20:40:19 -0700

    Switched to PoseEstimator, odometry seems to be consistent

--- src/main/java/frc/robot/commands/shooter/ShooterSequence.java
+++ src/main/java/frc/robot/commands/shooter/ShooterSequence.java
@@ -1,8 +1,11 @@
 package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WrapperCommand;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.LimelightHelpers;
 import frc.robot.RobotContainer.LimeLightPipelines;
 import frc.robot.subsystems.Shooter;
 import frc.robot.commands.swerve.RotateToAprilTag;
@@ -15,23 +18,31 @@ import frc.robot.subsystems.Wrist.WristPosition;
 public class ShooterSequence extends SequentialCommandGroup {
     private Shooter shooter;
 
-    public ShooterSequence(Shooter mShooter, Delivery mDelivery, Intake mIntake, Wrist mWrist, SOTA_SwerveDrive mSwerve) {
-       this.shooter = mShooter; 
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(LimeLightPipelines.SPEAKER.id);
-       addCommands(
-            Commands.runOnce(() -> {mWrist.setDesiredPosition(WristPosition.REST);}, mWrist),
-            Commands.waitUntil(mWrist::atSetpoint),
-            // Commands.run(() -> {mIntake.outtake(); mDelivery.toIntake();}, mIntake, mWrist).until(mIntake::hasNote),
-            new RotateToAprilTag(mSwerve),
-            Commands.parallel(
-                Commands.run(() -> {
-                    mShooter.spinUpFlyWheel();
-                    mShooter.goToAngle();
-                }, mShooter),
-                Commands.waitUntil(this::isReadyToShoot).andThen(Commands.run(() -> {mIntake.intake(); mDelivery.toShooter();}, mIntake, mDelivery))
-            ),
-            Commands.waitSeconds(0.5).andThen(Commands.runOnce(() -> mShooter.stopFlyWheel(), mShooter))
-        );
+    public ShooterSequence(Shooter mShooter, Delivery mDelivery, Intake mIntake, Wrist mWrist,
+            SOTA_SwerveDrive mSwerve) {
+        this.shooter = mShooter;
+        LimelightHelpers.setPipelineIndex("", LimeLightPipelines.SPEAKER.id);
+        addCommands(
+                Commands.runOnce(() -> {
+                    mWrist.setDesiredPosition(WristPosition.REST);
+                }, mWrist),
+                Commands.waitUntil(mWrist::atSetpoint),
+                // Commands.run(() -> {mIntake.outtake(); mDelivery.toIntake();}, mIntake,
+                // mWrist).until(mIntake::hasNote),
+                new RotateToAprilTag(mSwerve),
+                Commands.parallel(
+                        Commands.run(() -> {
+                            mShooter.spinUpFlyWheel();
+                            mShooter.goToAngle();
+                        }, mShooter),
+                        Commands.waitUntil(this::isReadyToShoot).andThen(Commands.run(() -> {
+                            mIntake.intake();
+                            mDelivery.toShooter();
+                        }, mIntake, mDelivery))),
+                Commands.waitSeconds(0.5).andThen(Commands.runOnce(() -> {
+                    mShooter.stopFlyWheel();
+                    LimelightHelpers.setPipelineIndex("", LimeLightPipelines.MEGATAG.id);
+                }, mShooter)));
 
         addRequirements(mShooter, mDelivery, mIntake, mWrist);
     }
